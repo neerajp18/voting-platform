12520c4fbea3e1fe85401bf5a9bb62dd430ceb3b
NEERAJ P <neerajpneeraj18@gmail.com>
2026-02-08 01:03:23 +0530

    Fixed Google passport strategy

--- backend/passport/google.js
+++ backend/passport/google.js
@@ -1,33 +1,33 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const User = require("../models/User");
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "https://voting-platform.onrender.com/auth/google/callback"
     },
-    async (_, __, profile, done) => {
-      let user = await User.findOne({ email: profile.emails[0].value });
+    (accessToken, refreshToken, profile, done) => {
 
-      if (!user) {
-        user = await User.create({
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          provider: "google"
-        });
-      }
+      const user = {
+        id: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value
+      };
 
-      done(null, user);
+      return done(null, user);
     }
   )
 );
 
-passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id);
+
+// serialize user
+passport.serializeUser((user, done) => {
   done(null, user);
 });
 
+// deserialize user
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
